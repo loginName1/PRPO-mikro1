7d703fc457eb7bda476e48cc7400ce8a888e2c65
Jan Benko <jb3894@student.uni-lj.si>
2021-12-13 20:28:00 +0100

    Application file

---
+++ src/main/java/si/fri/prpo/polnilnepostaje/katalogpostaj/api/v1/Application.java
@@ -0,0 +1,8 @@
+package si.fri.prpo.polnilnepostaje.katalogpostaj.api.v1;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("v1")
+public class Application extends javax.ws.rs.core.Application {
+
+}
